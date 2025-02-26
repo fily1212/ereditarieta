6ac6e73674d003dce03c585561f3935d051fb072
Filippo <hu.zhonglifilippo@itisgrassi.edu.it>
2025-02-26 09:09:05 +0100

    progetto

---
+++ src/ereditarieta/Main.java
@@ -0,0 +1,8 @@
+package ereditarieta;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona("Daniele", "Saraceno");
+        System.out.println(p);
+    }
+}
