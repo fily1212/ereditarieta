0f346bb0408656d373220c03e334e81d38a5f52e
Filippo <hu.zhonglifilippo@itisgrassi.edu.it>
2025-02-26 09:25:00 +0100

    Studente

--- src/ereditarieta/Main.java
+++ src/ereditarieta/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Persona p = new Persona("Daniele", "Saraceno");
         System.out.println(p);
+        Studente s = new Studente("Giorgia", "Riva", "4L");
+        System.out.println(s);
     }
 }
